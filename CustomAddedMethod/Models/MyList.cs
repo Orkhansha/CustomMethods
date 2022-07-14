ae18d94cead1bafa52c296f13e5e94541f76443f
Orkhansha <orkhansha@code.edu.az>
2022-07-14 06:58:52 +0400

    Added Custom Methods

---
+++ CustomAddedMethod/Models/MyList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomAddedMethod.Models
+{
+    public class MyLIst<T>
+    {
+        public string[] arr;
+
+        public MyLIst()
+        {
+            arr = new string[0];
+        }
+
+        public void Add(string name)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = name;
+        }
+
+        public void Clear(string name)
+        {
+            foreach (var item in arr)
+            {
+                if (item != name)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+        }
+
+
+        public void GetAll()
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ CustomAddedMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using CustomAddedMethod.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CustomAddedMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyLIst<string> studentList = new MyLIst<string>();
+
+            //studentList.Add("Orkhan");
+            //studentList.Add("Sabir");
+            //studentList.Add("Fidan");
+            //studentList.Add("Emil");
+
+
+            //studentList.Clear("Sabir");
+
+            //studentList.Find("Seid");
+
+            
+            
+        }
+    }
+    
+}
